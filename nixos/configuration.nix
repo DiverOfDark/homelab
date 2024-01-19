28480982e23e14cde8f875eb6e711de8a93bf17f
Kirill Orlov <me@kirillorlov.pro>
2024-01-19 22:27:28 +0100

    removed docker

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -35,7 +35,7 @@
   users.users = {
     diverofdark = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
       packages = with pkgs; [ ];
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCz5Mz8hwMB+mmNPB3g8NLUNq5SoWzoXRxOlSDQKpaGcPJCJaemXf3BVMFHdy/6pBnsrsD/aaA80ouJd40cu6jEdwy/9Z6VD65N8feNzV2xcdRoHUdAHyjyICkipX8kBTvhU4RznoGQFhunqwORuAhE4+7pEXyRHd/PUfKreYAXVuMGmh1Cs5WHmNLPje1VBdg4BgL9kSZbCkKlUsAp5KeAzvRHnJaCNAUkARDD4Bjdcm36uyctIg06AEMpun6GcEmMGSLoCwjl8S4SUCYbNbXK4mPmrGKS2Vm+tSbc3VH7JZwynHC8HQcAFiuQcXXM/j7UxWCyr8huvPTKILRIaZQZ diverofdark"
@@ -99,17 +99,6 @@
   };
   nixpkgs.config.allowUnfree = true;
   
-  virtualisation = {
-    containers = {
-      enable = true;
-    };
-    docker = {
-      enable = true;
-      logDriver = "json-file";
-    };
-    oci-containers.backend = "docker";
-  };
-
   systemd.extraConfig = ''
     DefaultTimeoutStartSec=300s
   '';
