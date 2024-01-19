702d2460d21ab9bdac5072ca7ca1aa90b0cbc157
Kirill Orlov <me@kirillorlov.pro>
2024-01-19 01:46:39 +0100

    increased systemd timeout for k3s to start

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -110,6 +110,10 @@
     oci-containers.backend = "docker";
   };
 
+  systemd.extraConfig = ''
+    DefaultTimeoutStartSec=300s
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
