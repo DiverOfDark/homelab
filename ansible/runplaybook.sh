d8fff9242f8f42c770bde0c650530b8a89d4b3fa
Kirill Orlov <me@kirillorlov.pro>
2024-06-10 22:30:12 +0200

    Adding playbook for k3s

--- ansible/runplaybook.sh
+++ ansible/runplaybook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source .venv/bin/activate
 
-ANSIBLE_CONFIG=ansible.cfg ansible-playbook playbook/playbook.yaml -i inventory.yaml
+ANSIBLE_CONFIG=ansible.cfg ansible-playbook playbook/playbook.yaml -i inventory.yaml  -J
+#--check --diff
