823bbd5578ffab53dea33d04f6ac7b4c58600d11
Kirill Orlov <me@kirillorlov.pro>
2024-07-27 00:42:24 +0200

    cleanup and split k3s yamls from manual cluster init to autosynced

--- k3s-bootstrap/apply.sh
+++ k3s-bootstrap/apply.sh
@@ -1,3 +1,7 @@
+#!/bin/bash;
 kubectl apply -f argocd-ns.yaml
-kubectl apply -f argocd.yaml -n argocd
+
+helm template argocd -n argocd oci://ghcr.io/argoproj/argo-helm/argo-cd --version 7.3.11 -f .\argocd-values.yaml > argocd.yaml
+kubectl apply -f argocd.yaml
+
 kubectl apply -f metaapp.yaml -n argocd
