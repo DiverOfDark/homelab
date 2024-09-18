ee74c7e911e2bf4b94d8211d322c078edaa21d45
Kirill Orlov <me@kirillorlov.pro>
2024-09-18 22:50:52 +0200

    extracted role for seedbox

--- ansible/runplaybook.sh
+++ ansible/runplaybook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .venv/bin/activate
 
-ANSIBLE_CONFIG=ansible.cfg ansible-playbook playbook/playbook.yaml -i inventory.yaml -J
+ANSIBLE_CONFIG=ansible.cfg ansible-playbook playbook/playbook.yaml -i inventory.yaml -J --tags $1
 #--check --diff
