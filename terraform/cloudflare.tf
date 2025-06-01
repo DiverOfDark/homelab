11ac8e825930041b5a3faee6c1dba96fb4fa7692
Kirill Orlov <me@kirillorlov.pro>
2025-06-01 03:04:46 +0200

    expose logto

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -75,6 +75,10 @@ locals {
       hostname = "dex.kirillorlov.pro"
       service  = "http://dex.oauth2-proxy.svc.cluster.local:5556"
     }, {
+      short = "logto"
+      hostname = "logto.kirillorlov.pro"
+      service  = "http://logto.logto.svc.cluster.local:3001"
+    }, {
       short = "ai"
       hostname = "ai.kirillorlov.pro"
       service  = "http://lobechat.lobechat.svc.cluster.local:3210"
