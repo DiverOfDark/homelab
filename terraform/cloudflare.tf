1bf9239e9be66222c4b66af2f973a078b75073a3
Kirill Orlov <me@kirillorlov.pro>
2026-03-01 23:45:38 +0100

    switch to terraform cloudflare v5

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -2,8 +2,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "registry.terraform.io/cloudflare/cloudflare"
-      version = "4.52.5"
-      #version = "5.0.0-alpha1"
+      version = "5.18.0"
     }
     kubernetes = {
       source  = "registry.opentofu.org/hashicorp/kubernetes"
@@ -112,54 +111,55 @@ provider "kubernetes" {
 
 resource "cloudflare_account" "account" {
   name = "kirillorlov.pro"
-  type = "standard"
 }
 
 resource "cloudflare_account_member" "main_account" {
-  account_id    = cloudflare_account.account.id
-  email_address = "diverofdark@gmail.com"
-  role_ids = ["33666b9c79b9a5273fc7344ff42f953d"]
-  status        = "accepted"
+  account_id = cloudflare_account.account.id
+  email      = "diverofdark@gmail.com"
+  roles      = ["33666b9c79b9a5273fc7344ff42f953d"]
+  status     = "accepted"
 }
 
 resource "cloudflare_zone" "zone" {
-  account_id = cloudflare_account.account.id
-  zone       = "kirillorlov.pro"
-  type       = "full"
+  account = {
+    id = cloudflare_account.account.id
+  }
+  name = "kirillorlov.pro"
+  type = "full"
 }
 
-resource "cloudflare_record" "records" {
+resource "cloudflare_dns_record" "records" {
   for_each = { for rules in local.ingress_rules: rules.short => rules }
   name    = each.value.short
 
   zone_id = cloudflare_zone.zone.id
   type    = "CNAME"
-  content = cloudflare_zero_trust_tunnel_cloudflared.kubernetes_account.cname
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.kubernetes_account.id}.cfargotunnel.com"
   ttl     = 1
   proxied = true
 }
 
-resource "cloudflare_record" "backup" {
+resource "cloudflare_dns_record" "backup" {
   name    = "yggdrasil"
 
   zone_id = cloudflare_zone.zone.id
   type    = "CNAME"
-  content = cloudflare_zero_trust_tunnel_cloudflared.yggdrasil.cname
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.yggdrasil.id}.cfargotunnel.com"
   ttl     = 1
   proxied = true
 }
 
-resource "cloudflare_record" "backup-s" {
+resource "cloudflare_dns_record" "backup-s" {
   name    = "yggdrasil-s"
 
   zone_id = cloudflare_zone.zone.id
   type    = "CNAME"
-  content = cloudflare_zero_trust_tunnel_cloudflared.yggdrasil.cname
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.yggdrasil.id}.cfargotunnel.com"
   ttl     = 1
   proxied = true
 }
 
-resource "cloudflare_record" "uptime1" {
+resource "cloudflare_dns_record" "uptime1" {
     name = "uptime"
     zone_id = cloudflare_zone.zone.id
     type = "CNAME"
@@ -169,7 +169,6 @@ resource "cloudflare_record" "uptime1" {
 }
 #todo setup email routing
 resource "cloudflare_email_routing_settings" "email_routing" {
-  enabled = true
   zone_id = cloudflare_zone.zone.id
 }
 
@@ -178,16 +177,16 @@ resource "cloudflare_email_routing_rule" "i_at_kirillorlov_pro" {
   name    = "i@kirillorlov.pro"
   enabled = true
 
-  matcher {
+  matchers = [{
     type  = "literal"
     field = "to"
     value = "i@kirillorlov.pro"
-  }
+  }]
 
-  action {
+  actions = [{
     type  = "forward"
     value = ["diverofdark@gmail.com"]
-  }
+  }]
 }
 
 resource "cloudflare_email_routing_rule" "me_at_kirillorlov_pro" {
@@ -195,16 +194,16 @@ resource "cloudflare_email_routing_rule" "me_at_kirillorlov_pro" {
   name    = "me@kirillorlov.pro"
   enabled = true
 
-  matcher {
+  matchers = [{
     type  = "literal"
     field = "to"
     value = "me@kirillorlov.pro"
-  }
+  }]
 
-  action {
+  actions = [{
     type  = "forward"
     value = ["diverofdark@gmail.com"]
-  }
+  }]
 }
 
 resource "cloudflare_email_routing_catch_all" "send_all_to_gmail" {
@@ -212,17 +211,17 @@ resource "cloudflare_email_routing_catch_all" "send_all_to_gmail" {
   name    = "catch all"
   enabled = true
 
-  matcher {
+  matchers = [{
     type = "all"
-  }
+  }]
 
-  action {
+  actions = [{
     type  = "forward"
     value = ["diverofdark@gmail.com"]
-  }
+  }]
 }
 
-resource "cloudflare_record" "dmarc" {
+resource "cloudflare_dns_record" "dmarc" {
   zone_id = cloudflare_zone.zone.id
   content = "v=DMARC1; p=none; rua=mailto:aae0da00a1dc4478bfd42740df319d8f@dmarc-reports.cloudflare.net,mailto:dmarc@kirillorlov.pro; aspf=r;"
   name    = "_dmarc"
@@ -231,7 +230,7 @@ resource "cloudflare_record" "dmarc" {
   type    = "TXT"
 }
 
-resource "cloudflare_record" "mailgun-txt" {
+resource "cloudflare_dns_record" "mailgun-txt" {
   zone_id = cloudflare_zone.zone.id
   content = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8FNoH2XVc2hlcoBoalxE9K1Q71wEGvwS2gtQMMO2yIq6cb+O/aJktZl1LtxOgabmOAyCSOXt2fVbwcZHo4+t+6E4JLHCUQUQ4YAAt4qRp/kdCG/HRi2de768tMUWloOPI4a4/66RxxbQIdwOsUN+MqMP9CIT6zarfOutsDW73vwIDAQAB"
   name    = "s1._domainkey.kirillorlov.pro"
@@ -240,7 +239,7 @@ resource "cloudflare_record" "mailgun-txt" {
   type    = "TXT"
 }
 
-resource "cloudflare_record" "spf" {
+resource "cloudflare_dns_record" "spf" {
   zone_id = cloudflare_zone.zone.id
   content = "v=spf1 a mx include:_spf.google.com include:_spf.mx.cloudflare.net include:mailgun.org ~all"
   name    = "kirillorlov.pro"
@@ -249,7 +248,7 @@ resource "cloudflare_record" "spf" {
   type    = "TXT"
 }
 
-resource "cloudflare_record" "email-cname" {
+resource "cloudflare_dns_record" "email-cname" {
   zone_id = cloudflare_zone.zone.id
   content = "eu.mailgun.org"
   name    = "email.kirillorlov.pro"
@@ -262,7 +261,7 @@ resource "cloudflare_zero_trust_access_identity_provider" "github" {
   account_id = cloudflare_account.account.id
   name       = "GitHub"
   type       = "github"
-  config {
+  config = {
     client_id = "Ov23liDFOC6cLvQ2YZM2"
   }
 }
@@ -271,6 +270,18 @@ resource "cloudflare_zero_trust_access_identity_provider" "onetime" {
   account_id = cloudflare_account.account.id
   type       = "onetimepin"
   name       = "One-Time PIN"
+  config     = {}
+}
+
+resource "cloudflare_zero_trust_access_policy" "warp_allow" {
+  account_id = cloudflare_account.account.id
+  name       = "Allow company emails"
+  decision   = "allow"
+  include = [{
+    email_domain = {
+      domain = "kirillorlov.pro"
+    }
+  }]
 }
 
 resource "cloudflare_zero_trust_access_application" "warp_enrollment_app" {
@@ -280,19 +291,11 @@ resource "cloudflare_zero_trust_access_application" "warp_enrollment_app" {
   allowed_idps              = [cloudflare_zero_trust_access_identity_provider.github.id, cloudflare_zero_trust_access_identity_provider.onetime.id]
   auto_redirect_to_identity = false
   type                      = "warp"
-  app_launcher_visible      = false
-}
 
-resource "cloudflare_zero_trust_access_policy" "warp_enrollment_employees" {
-  application_id = cloudflare_zero_trust_access_application.warp_enrollment_app.id
-  account_id     = cloudflare_account.account.id
-  name           = "Allow company emails"
-  decision       = "allow"
-  precedence     = 1
-
-  include {
-    email_domain = ["kirillorlov.pro"]
-  }
+  policies = [{
+    id         = cloudflare_zero_trust_access_policy.warp_allow.id
+    precedence = 1
+  }]
 }
 
 resource "cloudflare_zero_trust_dns_location" "default_location" {
@@ -311,11 +314,11 @@ resource "cloudflare_zero_trust_gateway_policy" "allow_home_network" {
   name        = "allow localnet"
   precedence  = 9
   traffic     = "net.dst.ip in {192.168.0.0/16}"
-  rule_settings {
+  rule_settings = {
     block_page_enabled                 = false
     insecure_disable_dnssec_validation = false
     ip_categories                      = false
-    notification_settings {
+    notification_settings = {
       enabled = false
     }
   }
@@ -324,48 +327,51 @@ resource "cloudflare_zero_trust_gateway_policy" "allow_home_network" {
 resource "cloudflare_zero_trust_tunnel_cloudflared" "kubernetes_account" {
   account_id = cloudflare_account.account.id
   name       = "k8s"
-  secret     = base64encode(var.tunnel_secret)
+  tunnel_secret = base64encode(var.tunnel_secret)
   config_src = "cloudflare"
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "yggdrasil" {
   account_id = cloudflare_account.account.id
   name       = "yggdrasil"
-  secret     = base64encode(var.tunnel_secret)
+  tunnel_secret = base64encode(var.tunnel_secret)
   config_src = "cloudflare"
 }
 
-resource "cloudflare_zero_trust_tunnel_virtual_network" "kubernetes_virtual_network" {
+resource "cloudflare_zero_trust_tunnel_cloudflared_virtual_network" "kubernetes_virtual_network" {
   account_id         = cloudflare_account.account.id
   name               = "HomeNetwork"
   is_default_network = true
 }
 
-resource "cloudflare_zero_trust_tunnel_route" "example" {
+resource "cloudflare_zero_trust_tunnel_cloudflared_route" "example" {
   account_id         = cloudflare_account.account.id
   tunnel_id          = cloudflare_zero_trust_tunnel_cloudflared.kubernetes_account.id
   network            = "192.168.0.0/16"
   comment            = "HomeNetwork route"
-  virtual_network_id = cloudflare_zero_trust_tunnel_virtual_network.kubernetes_virtual_network.id
+  virtual_network_id = cloudflare_zero_trust_tunnel_cloudflared_virtual_network.kubernetes_virtual_network.id
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "yggdrasil_config" {
   account_id = cloudflare_account.account.id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.yggdrasil.id
 
-  config {
-    ingress_rule {
-      hostname = "yggdrasil.kirillorlov.pro"
-      service  = "http://127.0.0.1"
-    }
-    ingress_rule {
-      hostname = "yggdrasil-s.kirillorlov.pro"
-      service  = "ssh://127.0.0.1"
-    }
-    ingress_rule {
-      # Respond with a `404` status code when the request does not match any of the previous hostnames.
-      service  = "http_status:404"
-    }
+  config = {
+    ingress = [
+      {
+        hostname = "yggdrasil.kirillorlov.pro"
+        service  = "http://127.0.0.1"
+      },
+      {
+        hostname = "yggdrasil-s.kirillorlov.pro"
+        service  = "ssh://127.0.0.1"
+      },
+      {
+        # Respond with a `404` status code when the request does not match any of the previous hostnames.
+        service = "http_status:404"
+      }
+    ]
+    origin_request = {}
   }
 }
 
@@ -373,19 +379,19 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "kubernetes_account_c
   account_id = cloudflare_account.account.id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.kubernetes_account.id
 
-  config {
-    dynamic "ingress_rule" {
-      for_each = local.ingress_rules
-      content {
-        hostname = ingress_rule.value.hostname
-        service  = ingress_rule.value.service
-      }
-    }
-    ingress_rule {
-      # Respond with a `404` status code when the request does not match any of the previous hostnames.
-      service  = "http_status:404"
-    }
-    warp_routing {
+  config = {
+    ingress = concat(
+      [for rule in local.ingress_rules : {
+        hostname = rule.hostname
+        service  = rule.service
+      }],
+      [{
+        # Respond with a `404` status code when the request does not match any of the previous hostnames.
+        service = "http_status:404"
+      }]
+    )
+    origin_request = {}
+    warp_routing = {
       enabled = true
     }
   }
@@ -400,7 +406,11 @@ resource "kubernetes_secret_v1" "cloudflared_config" {
     account-id = cloudflare_account.account.id
     api-token = var.cloudflare_api_token
     tunnel-id = cloudflare_zero_trust_tunnel_cloudflared.kubernetes_account.id
-    tunnelToken = cloudflare_zero_trust_tunnel_cloudflared.kubernetes_account.tunnel_token
+    tunnelToken = base64encode(jsonencode({
+      a = cloudflare_account.account.id
+      t = cloudflare_zero_trust_tunnel_cloudflared.kubernetes_account.id
+      s = base64encode(var.tunnel_secret)
+    }))
   }
 }
 
