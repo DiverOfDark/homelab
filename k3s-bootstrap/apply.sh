9962f3380b201b5dfbd21c4dd6f5649fcbab0fe6
Kirill Orlov <me@kirillorlov.pro>
2026-03-09 23:09:36 +0100

    update ArgoCD Helm chart to version 9.4.7 and enable server-side apply

--- k3s-bootstrap/apply.sh
+++ k3s-bootstrap/apply.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kubectl apply -f argocd-ns.yaml
 
-helm template argocd -n argocd oci://ghcr.io/argoproj/argo-helm/argo-cd --version 7.3.11 -f .\argocd-values.yaml > argocd.yaml
-kubectl apply -f argocd.yaml
+helm template argocd -n argocd oci://ghcr.io/argoproj/argo-helm/argo-cd --version 9.4.7 -f argocd-values.yaml > argocd.yaml
+kubectl apply -f argocd.yaml --server-side
 
 kubectl apply -f metaapp.yaml -n argocd
